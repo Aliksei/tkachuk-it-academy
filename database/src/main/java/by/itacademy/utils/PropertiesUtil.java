2047cd6b2d2f92d454283af1598c85aa3b4c81f9
Aliksei <kikit228@gmail.com>
2018-10-15 01:27:43 +0300

    property file renamed

--- database/src/main/java/by/itacademy/utils/PropertiesUtil.java
+++ database/src/main/java/by/itacademy/utils/PropertiesUtil.java
@@ -20,7 +20,7 @@ public final class PropertiesUtil {
     private static void loadProperties() {
         try {
             properties = new Properties();
-            properties.load(Files.newBufferedReader(Paths.get("C:\\Users\\Aliksei\\IdeaProjects\\jd2-project\\database\\src\\main\\resources\\app.properties")));
+            properties.load(Files.newBufferedReader(Paths.get("C:\\Users\\Aliksei\\IdeaProjects\\jd2-project\\database\\src\\main\\resources\\database.properties")));
         } catch (IOException e) {
             e.printStackTrace();
         }
